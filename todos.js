2649c446317e1a1f9bfe66d3e1a4e0276a5a3d2a
Jon Langlois <jonathan-langlois@live.ca>
2020-08-16 08:41:00 -0700

    tests

--- todos.js
+++ todos.js
@@ -3,16 +3,4 @@ module.exports = [
     id: 1,
     task: "Fix Sink"
   },
-  {
-    id: 2,
-    task: "Buy Groceries"
-  },
-  {
-    id: 3,
-    task: "Wash the dishes"
-  },
-  {
-    id: 4,
-    task: "Walk the cat"
-  },
 ];
